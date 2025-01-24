654a15a90aa91604d259c086828d114fff444a36
GleidesonFr <gleidesonfreitas015@gmail.com>
2025-01-24 17:27:53 -0300

    dinamic images in background at the cards has neen created.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-const cardImage = document.querySelector('.card-image');
-let card = document.querySelector('.card');
+const images = [
+    '/img/manuel-moreno-DGa0LQ0yDPc-unsplash.jpg',
+    '/img/matt-nelson-aI3EBLvcyu4-unsplash.jpg',
+    '/img/luca-bravo-O453M2Liufs-unsplash.jpg'
+];
 
-card.addEventListener('hover', function(){
+const bgImage = document.getElementById('background-image');
+const cards = document.querySelectorAll('#cards');
 
-});
+cards.forEach((box, index) => {
+    
+    box.addEventListener('mouseenter', () => {
+        bgImage.src = images[index];
+        bgImage.style.opacity = '1';
+    });
+  
+    box.addEventListener('mouseleave', () => {
+        bgImage.style.opacity = '0.5';
+    });
+  });
+  
